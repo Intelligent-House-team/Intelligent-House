bcff41b7d931ed230c54138ceafa8b188800e5da
Peinoi <ailis635@naver.com>
2025-04-10 16:28:37 +0900

    setting

---
+++ Intelligent-House/src/main/java/kr/co/Intelligent/House/IntelligentHouseApplication.java
@@ -0,0 +1,13 @@
+package kr.co.Intelligent.House;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentHouseApplication.class, args);
+	}
+
+}
---
+++ Intelligent-House/src/test/java/kr/co/Intelligent/House/IntelligentHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.Intelligent.House;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
