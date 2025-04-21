ce19f76336ff7c0ee66820981cde0f90288da725
김현태 <kht082@naver.com>
2025-04-22 02:12:51 +0900

    Update
    고정형 사이드바 추가, 기존의 히든 사이드바 스크립트 분리
    TODO: 고정형 사이드바, 헤더 펼치기 접기 버튼 추가해야됨

--- Intelligent-House/src/main/resources/static/js/side.js
+++ Intelligent-House/src/main/resources/static/js/side.js
@@ -1,4 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+fetch('/sidebar.html')
+  .then(res => res.text())
+  .then(html => {
+    document.body.insertAdjacentHTML('beforeend', html); // body 맨 마지막에 삽입
+
+    // sidebar.html 이 body에 삽입된 이후에 실행!
     const sidebarButton = document.getElementById('sidebarButton');
     const sidebar = document.getElementById('sidebar');
     const overlay = document.getElementById('overlay');
@@ -12,4 +17,24 @@ document.addEventListener('DOMContentLoaded', function() {
         sidebar.classList.remove('open');
         overlay.classList.remove('open');
     });
-});
+
+    // 고정형 사이드바 관련
+    const fixedSidebar = document.getElementById('fixed-sidebar');
+        const toggleButton = document.getElementById('toggle-sidebar-button');
+        const closeFixedSidebarButton = document.getElementById('closeFixedSidebarButton');
+
+        let isFixedSidebarOpen = true;
+
+        toggleButton.addEventListener('click', () => {
+          if (isFixedSidebarOpen) {
+            fixedSidebar.style.right = '-300px'; // 숨김
+          } else {
+            fixedSidebar.style.right = '0';
+          }
+          isFixedSidebarOpen = !isFixedSidebarOpen;
+        });
+
+        closeFixedSidebarButton.addEventListener('click', function() {
+            fixedSidebar.style.display = 'none';
+        });
+  });
