19e6f945ed453015660e5386c96c8a1f04c1c5c6
Peinoi <124995489+Peinoi@users.noreply.github.com>
2025-04-24 15:20:26 +0900

    api
    위성 지도 변경 코드

--- Intelligent-House/src/main/resources/static/js/app.js
+++ Intelligent-House/src/main/resources/static/js/app.js
@@ -5,3 +5,14 @@ var options = { //지도를 생성할 때 필요한 기본 옵션
 };
 
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+let isHybrid = false; // 현재 상태 추적
+
+function toggleMapType() {
+    if (isHybrid) {
+        map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP); // 일반 지도
+    } else {
+        map.setMapTypeId(kakao.maps.MapTypeId.HYBRID); // 위성 지도 + 도로 정보
+    }
+    isHybrid = !isHybrid;
+}
