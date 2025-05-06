c64d62e3b2d0e06f5ece0d6ad7728f59bde47557
김현태 <kht082@naver.com>
2025-05-06 21:18:28 +0900

    Update
    header.html: 버튼/css 추가
    app.js: map을 전역변수로 변경하고 임시 버튼 제거
    loade-header.js: 레이아웃 버튼 2개의 상호작용 이벤트 추가
    index.html: js파일 일부 수정하면서 서순에 문제가생긴것 같아 <script> 태그의 순서 일부 변경

--- Intelligent-House/src/main/resources/static/js/app.js
+++ Intelligent-House/src/main/resources/static/js/app.js
@@ -4,15 +4,5 @@ var options = { //지도를 생성할 때 필요한 기본 옵션
 	level: 3 //지도의 레벨(확대, 축소 정도)
 };
 
-var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-
-let isHybrid = false; // 현재 상태 추적
-
-function toggleMapType() {
-    if (isHybrid) {
-        map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP); // 일반 지도
-    } else {
-        map.setMapTypeId(kakao.maps.MapTypeId.HYBRID); // 위성 지도 + 도로 정보
-    }
-    isHybrid = !isHybrid;
-}
+//var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+window.map = new kakao.maps.Map(container, options); //전역변수 지도 생성 및 객체 리턴
--- Intelligent-House/src/main/resources/static/js/load-header.js
+++ Intelligent-House/src/main/resources/static/js/load-header.js
@@ -8,4 +8,35 @@ fetch('header.html')
     const title = 'Intelligent House';
     document.getElementById('site-title').textContent = title;
     document.title = title;
+
+    // 타이틀과 함께 움직일 페이지 레이아웃 변경 버튼
+    const buttons = document.querySelectorAll('#layoutGroup-View .layout-button');
+    let currentType = 'map'; // 기본 상태
+
+    buttons.forEach(btn => {
+          btn.addEventListener('click', () => {
+            const selectedType = btn.id === 'layout-MapView' ? 'map' : 'sky';
+
+            if (selectedType === currentType) {
+              return; // 같은 버튼 눌렀을 땐 아무 일도 안 함
+            }
+
+            // 버튼 상태 업데이트
+            buttons.forEach(b => b.classList.remove('selected'));
+            btn.classList.add('selected');
+
+            // 맵 타입 전환
+            currentType = selectedType;
+            toggleMapType(currentType);//여기 아니면
+            console.log("text: "+currentType);
+          });
+        });
+
+    function toggleMapType(type) {
+        if (type === 'map') {
+            window.map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP); // 일반 지도
+        } else {
+            window.map.setMapTypeId(kakao.maps.MapTypeId.HYBRID); // 위성 지도 + 도로 정보
+        }
+    }
   });
