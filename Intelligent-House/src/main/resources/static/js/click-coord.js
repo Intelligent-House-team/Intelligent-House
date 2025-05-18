ea626b8a9e30531f6755aaf340c80cbbf61ec6c3
Peinoi <ailis635@naver.com>
2025-05-18 17:39:10 +0900

    double click
    더블 클릭시 지도 확대

--- Intelligent-House/src/main/resources/static/js/click-coord.js
+++ Intelligent-House/src/main/resources/static/js/click-coord.js
@@ -1,4 +1,4 @@
-kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
-    const latlng = mouseEvent.latLng;
-    alert('클릭한 위치\n위도: ' + latlng.getLat() + '\n경도: ' + latlng.getLng());
+kakao.maps.event.addListener(map, 'dblclick', function(mouseEvent) {
+    // 더블클릭한 위치를 중심으로 지도를 확대합니다
+    map.setLevel(map.getLevel() - 1, { anchor: mouseEvent.latLng });
 });
